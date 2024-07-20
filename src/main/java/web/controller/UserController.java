9054e134970176eb8ca0d344f16c9a3ff306322e
miakEle <myakushkinaea@gmail.com>
2024-07-20 20:42:16 +0300

    Eri tavalla sama

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -19,18 +19,18 @@ public class UserController {
 
     @GetMapping("/")
     public String table(Model model) {
-        model.addAttribute("table", userService.getAllUsers());
+        model.addAttribute("allUsers", userService.getAllUsers());
         return "table";
     }
 
     @GetMapping("/new")
     public String addUser(Model model) {
-        model.addAttribute("user", new User());
+        model.addAttribute("newUser", new User());
         return "create";
     }
 
     @PostMapping("/new")
-    public String add(@ModelAttribute("user") User user, BindingResult bindingResult) {
+    public String add(User user, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return "create";
         } else {
@@ -47,16 +47,16 @@ public class UserController {
 
     @GetMapping("/edit")
     public String updateUser(@RequestParam int id, Model model) {
-        model.addAttribute("user", userService.getUserById(id));
+        model.addAttribute("userToUpdate", userService.getUserById(id));
         return "edit";
     }
 
     @PostMapping("/edit")
-    public String update(User user, BindingResult bindingResult) {
+    public String update(User userToSave, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return "edit";
         } else {
-            userService.updateUser(user);
+            userService.updateUser(userToSave);
             return "redirect:/";
         }
     }
